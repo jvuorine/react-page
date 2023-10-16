a71b1a0b71fd51bb6a0a96028e39c3914be7d312
jvuorine <vuorinen.jere@gmail.com>
2023-10-16 17:37:05 +0300

    setup gh-pages

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+export function About() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Test about
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+export function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test
+      </header>
+    </div>
+  );
+}
+
+export default App;
